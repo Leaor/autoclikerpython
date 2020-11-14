2196f111bb52e7a1297328ab053b4aa9728396cc
Bald Spaghetti <74433811+BaldSpaghetti@users.noreply.github.com>
2020-11-14 17:08:05 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AutoClicker',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/baldspaghetti',
+    license='',
+    author='BaldSpaghetti',
+    author_email='',
+    description='A simple autoclicker with controls using pyautogui'
+)
